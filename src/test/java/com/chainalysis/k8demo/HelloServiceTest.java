cae2d4105ae0ae8562ac75cd6de036e53f643b8b
Erik Martino <martino@chainalysis.com>
2020-09-27 14:28:34 +0200

    micronaut launch (2.1.0 snapshot, java, gradle, junit)

---
+++ src/test/java/com/chainalysis/k8demo/HelloServiceTest.java
@@ -0,0 +1,21 @@
+package com.chainalysis.k8demo;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class HelloServiceTest {
+
+    @Inject
+    EmbeddedApplication application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
