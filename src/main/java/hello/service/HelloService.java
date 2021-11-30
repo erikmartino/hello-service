2850a433e2f16aaa9c75bcf7c475bc02cbffc2e8
Erik Martino <martino@chainalysis.com>
2021-11-30 17:12:26 +0100

    wip

--- src/main/java/hello/service/HelloService.java
+++ src/main/java/hello/service/HelloService.java
@@ -1,4 +1,18 @@
 package hello.service;
 
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.validation.Validated;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+
+@Validated
+@Controller("/")
 public class HelloService {
+    @Get()
+    public Map<String,Object> getGreeting() throws UnknownHostException {
+        return Map.of("hostname", InetAddress.getLocalHost().getHostName());
+    }   
 }
